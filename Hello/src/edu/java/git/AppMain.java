05947a30185e72988de3b4a937659b9da7180371
bbongles <bbongles@naver.com>
2016-11-11 14:17:48 +0900

    Update AppMain.java

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,6 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 		System.out.println("안녕하세요~");
+		System.out.println("Another person modified this...");
 
 	} // end main()
 
