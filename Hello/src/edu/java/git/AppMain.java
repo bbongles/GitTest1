49b6e848c189147030b4c67d5ae8439020ba0bed
stu <stu@tiwill>
2016-11-11 14:14:16 +0900

    두번째 커밋(인코딩 타입 수정)

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -4,7 +4,7 @@ public class AppMain {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
-		System.out.println("¾È³çÇÏ¼¼¿ä~");
+		System.out.println("ì•ˆë…•í•˜ì„¸ìš”~");
 
 	} // end main()
 
