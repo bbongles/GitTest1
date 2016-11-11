7c50f13523e1354d806bfd638f49ffdae14fa2f3
stu <stu@tiwill>
2016-11-11 14:53:58 +0900

    내가 고침

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -7,6 +7,8 @@ public class AppMain {
 		System.out.println("안녕하세요~");
 		System.out.println("Another person modified this...");
 
+		System.out.println("새로운 기능 추가");
+		
 	} // end main()
 
 } // end class AppMain()
